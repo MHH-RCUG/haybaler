b83325fc46617772e716d5f031dc658df1ad28b4
Dr. Colin Davenport <davenport.colin@mh-hannover.de>
2021-02-24 11:50:58 +0100

    check Rscript present, add heatmaply heatmap lib

--- create_heatmap.R
+++ create_heatmap.R
@@ -1,7 +1,16 @@
 # R Script to create a heatmap after preparing the data with haybaler and the preparing script
 # Sophia Pörtner, Dec. 2020
+# heatmap requires base R
+# the interactive heatmaply variant requires heatmaply # install.packages(heatmaply)
 usage = "Usage: Rscript create_heatmap.R infile.csv"
 usage
+
+version = "0.11"
+version
+
+# changelog
+# 0.11  change colours
+# 0.10  add heatmaply interactive heatmap variant
 
 args <- commandArgs()
 #args
@@ -15,6 +24,8 @@ file
 path = "."              # Path to your data, default current dir "."
 directory = "."         # directory you want the heatmap to be saved in ("." is current dir)
 outputname = paste0(file,"_","heatmap.pdf")
+output_html = paste0(file,"_","heatmaply.html")
+output_png = paste0(file,"_","heatmaply.png")
 
 # Read the Data
 data = sprintf("%s/%s", path, file)
@@ -23,7 +34,7 @@ your_data = read.csv(data, sep="\t", header=TRUE, row.names = 1)
 # Make your Data numeric so the heatmap function can work with it
 your_data_2 = as.matrix(your_data)
 
-# create the heatmap
+# create the heatmap with base R
 # Change the size of the Rows (Species), and Columns (Samples) if needed
 # Try a few sizes and run this script again until the heatmap looks as you like it
 size_rows <- 0.6
@@ -45,4 +56,21 @@ heatmap(
         cexCol = size_columns
         )
 dev.off()
+
+# create an interactive HTML heatmap with heatmaply
+# Other color options see https://www.r-graph-gallery.com/38-rcolorbrewers-palettes.html
+# We don't want a divergent colour palette which accentuates deviations from the mean
+
+# install.packages(heatmaply)
+library(heatmaply)
+
+heatmaply(
+        your_data_2, 
+        #color = YlGn,
+        color = Blues,
+        #color = cool_warm,
+        #file=c(output_html, output_png)  #png needs another missing dependency
+        file=c(output_html)
+)
+
 
--- runbatch_create_heatmap.sh
+++ runbatch_create_heatmap.sh
@@ -4,12 +4,19 @@
 
 echo "INFO: Starting batch heatmap creation using create_heatmap.R"
 
-rscript_bin=/usr/bin/Rscript
-echo "Using rscript binary: " $rscript_bin
+# check for rscript, exit if unavailable
+rscript_bin="/usr/bin/Rscript"
+if [[ ! -f $rscript_bin ]]
+        then
+        echo "INFO: Rscript binary not found, aborting. Could not find this, is R installed? " $rscript_bin
+        exit
+fi
+echo "INFO: Using rscript binary: " $rscript_bin
 
+# create heatmaps for each heatmap.csv file
 for heatmapcsv in `ls *.heatmap.csv`
         do
-	echo "Creating heatmap for file: $heatmapcsv"
+	echo "INFO: Creating heatmap for file: $heatmapcsv"
         # run local
 	$rscript_bin create_heatmap.R $heatmapcsv
 done
