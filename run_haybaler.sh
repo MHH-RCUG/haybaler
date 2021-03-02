0ed701a5e22431e35cfb38d22d6690412ca7f51b
rcug_lw <dausterj>
2021-03-02 10:47:36 +0100

    copy scripts for heatmaps

--- run_haybaler.sh
+++ run_haybaler.sh
@@ -2,12 +2,26 @@
 
 echo "Starting Haybaler"
 
+# set directory to get the haybaler heatmaps scripts from
+# use default directory if no argument ($1) given
+if [ -z "$1" ]
+then
+  haybaler_dir=/mnt/ngsnfs/tools/dev/haybaler/  # default directory
+else
+  haybaler_dir="$1"
+fi
+echo "$haybaler_dir"
+
 outputDir=haybaler_output
 if [ ! -d $outputDir ]
 then
     echo "INFO: Creating directory:" $outputDir
     mkdir $outputDir
 fi
+
+cp $haybaler_dir/prepare_for_R_heatmap.sh $outputDir
+cp $haybaler_dir/runbatch_create_heatmap.sh $outputDir
+cp $haybaler_dir/create_heatmap.R $outputDir
 
 for csv in $(ls *.bam*.csv)
 do
