9f15cffb3d89ddaae25086c63d3ff44355b81f6a
Dr. Colin Davenport <davenport.colin@mh-hannover.de>
2021-03-03 15:11:38 +0100

    Separate default haybaler directory. Env var not yet implemented

--- run_haybaler.sh
+++ run_haybaler.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+# Sophia Poertner
+# Run haybaler https://github.com/MHH-RCUG/haybaler/
 
 echo "Starting Haybaler"
 
 # set directory to get the haybaler heatmaps scripts from
 # use default directory if no argument ($1) given
+
+# Users: change this to your haybaler path
+haybaler_directory="/mnt/ngsnfs/tools/dev/haybaler/" 
+
+
+# Users: don't modify this section
 if [ -z "$1" ]
 then
-  haybaler_dir=/mnt/ngsnfs/tools/dev/haybaler/  # default directory
+  haybaler_dir=$haybaler_directory
 else
   haybaler_dir="$1"
 fi
