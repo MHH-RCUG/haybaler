5a10b50e74933f5888986525644c4a86fb790208
Dr. Colin Davenport <davenport.colin@mh-hannover.de>
2021-03-06 19:10:21 +0100

    Add simple scale to base R heatmaps

--- prepare_for_R_heatmap.sh
+++ prepare_for_R_heatmap.sh
@@ -5,8 +5,9 @@
 
 echo "Usage: bash prepare_for_R_heatmap.sh"
 
-# change this to the number of taxa you want in the heatmap (just uses head -n X)
-number_of_taxa=20
+# change this to the number of taxa you want in the heatmap (just uses head -n X). 
+# 50 seems the max number where all labels on the y axis are still visible
+number_of_taxa=50
 
 for infile in `ls *haybaler.csv`
         do
@@ -15,8 +16,11 @@ for infile in `ls *haybaler.csv`
         #exclude mouse, human, mito
         grep -v "^chr" $infile | grep -v "^1_1_1" > $infile.filt.csv
 
-	# using tab delimiters, cut a max of 200 columns out excluding cols 2-3
-	cut -f1,4-200 -d"	" $infile.filt.csv | head -n $number_of_taxa > $infile.filt.heatmap.csv
+	# using tab delimiters, cut a max of 200 columns out excluding cols 2-3. Also restrict to number_of_taxa lines
+	cut -f1,4-200 -d"	" $infile.filt.csv | head -n $number_of_taxa > $infile.filt2.csv
+
+	# remove _complete_genome from labels
+	sed "s/_complete_genome//g" $infile.filt2.csv > $infile.filt.heatmap.csv
 
 	# shorten TODO names to 20 chars? awk ?
 	#$infile.filt.heatmap.csv > $infile.filt.heatmap.csv
