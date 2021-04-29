9ddb5771757895e4f35c00f24adbabc5629c6a35
poertnes <poertner.sophia@mh-hannover.de>
2021-04-29 13:46:46 +0200

    solved xxx out of range bug. Better output print for reference testing. Better run_haybaler_tax.sh script

--- haybaler_taxonomy.py
+++ haybaler_taxonomy.py
@@ -122,7 +122,7 @@ def find_double_taxid(df, taxonomy_name):
         if double_taxid:
             double_taxid = False
             continue
-        if index + 1 != len(df):
+        if index + 2 < len(df):
             # if three have the same Name but different TaxIDs
             if row["Name"] == df["Name"][index + 1] and row["Name"] == df["Name"][index + 2] and row["TaxID"] != \
                     df["TaxID"][index + 1] and row["TaxID"] != df["TaxID"][index + 2] and df["TaxID"][index + 2] != \
@@ -158,6 +158,23 @@ def find_double_taxid(df, taxonomy_name):
                     new_df = pd.concat([new_df, one_row])
             else:
                 new_df = new_df.append(row)
+        elif index + 1 < len(df):
+            # if two have the same Name but different TaxIDs
+            if row["Name"] == df["Name"][index + 1] and row["TaxID"] != df["TaxID"][index + 1]:
+                domain_1 = row["Lineage"].split(";")[0]
+                domain_2 = df["Lineage"][index + 1].split(";")[0]
+                double_taxid = True  # skip next iteration
+                # check if only one of the organisms is a bacteria. If yes, add this organism to the new df, else add
+                # just the name and leave the lineage empty
+                if domain_1 == "Bacteria" and domain_2 != "Bacteria":
+                    new_df = new_df.append(row)
+                elif domain_2 == "Bacteria" and domain_1 != "Bacteria":
+                    new_df = new_df.append(df.iloc[index + 1, :])
+                else:
+                    one_row = pd.DataFrame([row["Name"]], columns=["Name"])
+                    new_df = pd.concat([new_df, one_row])
+            else:
+                new_df = new_df.append(row)
         else:
             new_df = new_df.append(row)
     new_df = new_df.reset_index(drop=True)
@@ -170,7 +187,7 @@ def find_double_taxid(df, taxonomy_name):
         if index + 1 != len(new_df):
             if row["name2taxid_name"] == new_df["name2taxid_name"][index + 1] and row["Name"] != new_df["Name"][index + 1]:
                 if row["Name"] == row["name2taxid_name"]:
-                    df_wrong_organisms = df_wrong_organisms.append(new_df.iloc[index + 1, :])  # add wrong organisms to the df_wrong_organisms
+                    df_wrong_organisms = df_wrong_organisms.append(new_df.iloc[index + 1, :])  # add wrong organisms to df_wrong_organisms
                 if new_df["Name"][index + 1] == row["name2taxid_name"]:
                     df_wrong_organisms = df_wrong_organisms.append(row)  # add wrong organisms to the df_wrong_organisms
     wrong_ids = set(df_wrong_organisms["TaxID"].values.tolist())  # get the IDs for the wrong organisms
@@ -190,18 +207,19 @@ def save_csv(csv, path, name):
 @click.command()
 @click.option('--input_file', '-i', help='Name of the input file', required=True)
 @click.option('--input_path', '-p', help='Path of the input file, use . for current directory', required=True)
-def main(input_file, input_path):
+@click.option('--test_reference', '-t', help='Set to True if references should be tested. Default = False', default=False)
+def main(input_file, input_path, test_reference):
     # Mode for testing References. True or False
-    test_references = False
+    # test_references = True
     pd.set_option('display.max_rows', 100000)
-    csv = read_csv(input_file, input_path)
+    csv = pd.read_csv(input_path + "/" + input_file, sep="\t", index_col=0, header=None)
     # find_species(csv, input_file, input_path)  # work in progress
     genus = shorten_organism_names(csv)
     taxonomy = pytaxonkit.name2taxid(genus)
     taxids = taxonomy["TaxID"].to_list()
     lineage = pytaxonkit.lineage(taxids)
     filtered_csv = find_double_taxid(lineage, taxonomy["Name"])  # [["TaxID", "Name", "Lineage"]]
-    if not test_references:
+    if not test_reference:
         nan_list = set(taxonomy[taxonomy["TaxID"].isna()]["Name"].to_list())
         # replace every name that produces as NAN output in pytaxonkit with "NOT KNOWN"
         genus_less_nan = ["NOT KNOWN" if name in nan_list else name for name in genus]
@@ -213,19 +231,30 @@ def main(input_file, input_path):
     else:
         nan_list = set(taxonomy[taxonomy["TaxID"].isna()]["Name"].to_list())  # list of names that produce NAN
         genus_series = pd.Series(genus)
+        lineage_series = pd.Series(filtered_csv["Lineage"].values)
         csv.insert(loc=0, column='genus', value=genus_series.values)
+        csv.insert(loc=1, column='lineage', value=lineage_series.values)
+        csv["lineage"] = csv["lineage"].fillna(";;;;;;")
         # print(csv[csv['genus'].isin(nan_list)]["genus"])  # print everything that didn't work with pytaxonkit
         total_chr = len(genus)
         chr_not_work = len(taxonomy[taxonomy["TaxID"].isna()])
         chr_work = total_chr - chr_not_work
+        chr_no_lineage = csv.loc[csv["lineage"] == ";;;;;;"].shape[0]
+        chr_lineage = total_chr - chr_no_lineage
         print("reference tested:", input_file)
-        print(total_chr, "total chromosomes,", chr_work, "chromosomes were OK,", chr_not_work, "Did not work")
-        print(chr_work / total_chr, "of the reference works,", chr_not_work / total_chr, "Did not work")
+        print(total_chr, "total chromosomes,", "genus found for", chr_work, "chromosomes, no genus found for",
+              chr_not_work, "chromosomes")
+        print("Found genus for", chr_work / total_chr, "of the reference")
         if len(genus) == len(filtered_csv):
             print("The input and the output for lineage are the same. Filtering has probably worked.")
+            print(total_chr, "total chromosomes,", "lineage found for", chr_lineage, "chromosomes, no lineage found for",
+                  chr_no_lineage, "chromosomes")
+            print("Found lineage for", chr_lineage / total_chr, "of the reference")
+            print("")
         else:
-            print("The input is", len(genus), "organisms long. The output is", len(filtered_csv),
+            print("The input for lineage is", len(genus), "organisms long. The output is", len(filtered_csv),
                   "long. In the process of getting taxid, lineage and filtering something must have gone wrong")
+            print("")
             # compare the lineages in the different filter steps. Good for bug and reference testing
             # all_steps = pd.concat((pd.Series(csv.index.values), pd.Series(genus)), axis=1, ignore_index=True)
             # all_steps = pd.concat((all_steps, taxonomy[["Name", "TaxID"]]), axis=1, ignore_index=True)
