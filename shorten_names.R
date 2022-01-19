716373990db4816bac54a5873ba9dc6e97336c67
hollstel <89009355+LisaHollstein@users.noreply.github.com>
2022-01-19 14:09:58 +0100

    don't add rows, keep long name instead

--- shorten_names.R
+++ shorten_names.R
@@ -2,6 +2,7 @@
 # Lisa Hollstein, Dec 2021 - Jan 2022
 
 args <- commandArgs()
+args <- c("","","","","","/mnt/ngsnfs/gen/rcug_lw/Lisa/names","reads_per_million_reads_in_experiment_haybaler.csv.filt.heatmap.csv")
 
 print(args)
 
@@ -14,18 +15,21 @@ bacteria_data <- read.csv(input_file, sep = "\t", dec = ",", row.names = 1, head
 # create dataframe in which rownames are shortened
 sn_bacteria_data <- bacteria_data
 
+duplicates <- c()
+
 
 # check if new name already exists and rename the row
-check_existing <- function(new_name, old_name) {
+check_existing <- function(new_name, old_name, duplicates) {
   if(new_name %in% rownames(sn_bacteria_data)){
-    # add values of this row to values of existing row with the same short name
-    sn_bacteria_data[new_name,] <- as.numeric(sn_bacteria_data[new_name,]) + as.numeric(sn_bacteria_data[old_name,])
-    sn_bacteria_data <- sn_bacteria_data[rownames(sn_bacteria_data) != old_name,]
-    warning(paste(old_name,"has been added to", new_name))
-  } else{
+    # keep long name if short row name wouldn't be unique
+    place <- match("Botrytis cinerea", rownames(sn_bacteria_data))
+    rownames(sn_bacteria_data)[place] <- rownames(bacteria_data)[place]
+    duplicates <- c(duplicates, new_name)
+  } else if (!(new_name %in% duplicates)){
     rownames(sn_bacteria_data)[rownames(sn_bacteria_data) == old_name] <- new_name  # rename row
   }
-  return(sn_bacteria_data)
+  return_items <- list("duplicates" = duplicates, "data" = sn_bacteria_data)
+  return(return_items)
 }
 
 # add subspecies to new name
@@ -36,7 +40,7 @@ subspecies <- function(new_name, split_name) {
     a <- a + 1
     add <- split_name[n+a]
   }
-  new_name <- paste(new_name, "subsp", add)
+  new_name <- paste(new_name, "subsp", add, sep = "_")
   return(new_name)
 }
 
@@ -52,22 +56,26 @@ for (i in row_names){
     rest <- utf8ToInt(substring(split_name[n], 2))
     second_letter <- utf8ToInt(substring(split_name[n+1], 1, 1))
     if (split_name[n] == "organism" || split_name[n] == "candidatus"){
-      name <- paste(split_name[n], split_name[n+1], split_name[n+2])
+      new_name <- paste(split_name[n], split_name[n+1], split_name[n+2], sep = "_")
       if (split_name[n+3] == "subsp") {  # check for subspecies
-        name <- subspecies(name, split_name)
+        new_name <- subspecies(new_name, split_name)
       }
-      final_names <- c(final_names, name)
-      sn_bacteria_data <- check_existing(name, i)
+      final_names <- c(final_names, new_name)
+      return_items <- check_existing(new_name, i, duplicates)
+      sn_bacteria_data <- return_items$data
+      duplicates <- return_items$duplicates
       break
     }else if(length(first_letter) == 0 || length(second_letter) == 0 || length(rest) == 0){
     }else if(64 < first_letter && first_letter < 91 && 96 < second_letter && second_letter < 123){
-      name <- paste(split_name[n], split_name[n+1])
+      new_name <- paste(split_name[n], split_name[n+1], sep = "_")
       if (split_name[n+2] == "subsp"){  # check for subspecies
-        name <- subspecies(name, split_name)
+        new_name <- subspecies(new_name, split_name)
       }
       
-      final_names <- c(final_names,name)
-      sn_bacteria_data <- check_existing(name, i)
+      final_names <- c(final_names,new_name)
+      return_items <- check_existing(new_name, i, duplicates)
+      sn_bacteria_data <- return_items$data
+      duplicates <- return_items$duplicates
     
       break
     }
